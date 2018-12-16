0de756d7ab9d8e3a5d4f4c832ebc73367294b069
sahn <amirhossein3089@yahoo.com>
2018-12-16 21:50:24 +0330

    income in models and some cleanining

--- web/models.py
+++ web/models.py
@@ -7,3 +7,14 @@ class Expense(models.Model):
     date = models.DateTimeField()
     amount = models.BigIntegerField()
     user = models.ForeignKey(User,on_delete=models.CASCADE)
+    def __str__(self):
+        return "{}-{}".format(self.date,self.amount)
+
+class Income(models.Model):
+    text = models.CharField(max_length = 255)
+    date = models.DateTimeField()
+    amount = models.BigIntegerField()
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
+    def __str__(self):
+        return "{}-{}".format(self.date,self.amount)
+
